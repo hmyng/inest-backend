4431f59cdf32e6a9b2d640499cf5720c8493d5fc
hmyng <uonghongminh07@gmail.com>
2023-11-16 11:41:00 +0700

    test mongodb

--- src/app/server/dms/data.py
+++ src/app/server/dms/data.py
@@ -1,5 +1,5 @@
 from server.database.mongo import data_collection
 
 async def get_data():
-    res = data_collection.find_one()
+    res = await data_collection.find_one({}, {'_id': 0})
     return res
