7a67ed50a1294c89b5e9501fe27913f932b189d9
project-vaipe <projectvaipe@gmail.com>
2023-11-23 00:30:10 +0700

    update api

--- src/app/server/dms/data.py
+++ src/app/server/dms/data.py
@@ -1,12 +1,25 @@
-# from server.database.mongo import data_collection
+from server.database.mongo import data_collection
 import pandas as pd
 import math
 
-async def get_data(per_page: int=20, page: int=1, from_date="2021-01-01", to_date="2024-12-31"):
-    # res = await data_collection.find_one({}, {'_id': 0})
-    data = pd.read_csv('./server/data/210109.csv')
-    data['TheTime'] = pd.to_datetime(data['TheTime'])
-    data = data[(data['TheTime'] >= from_date) & (data['TheTime'] <= to_date)]
+async def get_data(per_page: int=20, page: int=1, from_date="2021-01-01", to_date="2024-12-31", device_id: int=1):
+    data = data_collection.find(
+        {"TheTime": {
+        '$gte': from_date,
+        '$lte': to_date,
+        }}
+        # {"device_id": device_id}
+    )
+    data_response = []
+    for i in data:
+        i['_id'] = str(i['_id'])
+        data_response.append(i)
+    data = pd.DataFrame(data_response)
+    if data.empty:
+        return []
+    # data = pd.read_csv('./server/data/210109.csv')
+    # data['TheTime'] = pd.to_datetime(data['TheTime'])
+    # data = data[(data['TheTime'] >= from_date) & (data['TheTime'] <= to_date)]
     meta = {
         "total": len(data),
         "total_page": math.ceil(len(data)/per_page),
